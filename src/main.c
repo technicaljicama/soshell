99799e78557631b61ae08c0cb9814d2c69554d74
JohnVictoryz <john.g.zioupos@gmail.com>
2023-03-21 20:03:18 +0200

    Added colors and working directory

--- src/main.c
+++ src/main.c
@@ -6,6 +6,14 @@
 #include <string.h>
 #include <sys/utsname.h>
 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 /*
   Function Declarations for builtin shell commands:
  */
@@ -243,13 +251,14 @@ void soshell_loop(void)
    perror("uname");
    exit(EXIT_FAILURE);
   }
- 
+  char workdir[100];
   char *line;
   char **args;
   int status;
 
   do {
-    printf("%s $ ", buffer.nodename);
+    printf(ANSI_COLOR_RED "%s" ANSI_COLOR_RESET,  buffer.nodename);
+    printf(ANSI_COLOR_GREEN " [%s]$ " ANSI_COLOR_RESET, getcwd(workdir, 100));
     line = soshell_read_line();
     args = soshell_split_line(line);
     status = soshell_execute(args);
